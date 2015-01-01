98a0f4a15c15e740f5640becd9c331bff026c48d
technologyRiff <technologyriff@gmail.com>
2014-12-31 20:44:03 -0500

    Fixed typo in production evironment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   # http://bloccit.herokuapp.com
-  config.action_mailer.defaul_url_options = { host: 'riff-bloccit.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'riff-bloccit.herokuapp.com' }
 
 end
