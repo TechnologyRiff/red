fef899e8e54c83f81b6112977c61cd6896468984
technologyRiff <technologyriff@gmail.com>
2015-01-02 15:36:14 -0500

    Validating posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,13 @@
 class PostsController < ApplicationController
-  def index
-  	@posts = Post.all
-  end
+  # def index
+  # 	@posts = Post.all
+  #   @topic = Topic.find(params[:topic_id])
+
+  # end
 
   def show
   	 @topic = Topic.find(params[:topic_id])
-     @post = Post.find(parms[:id])
+     @post = Post.find(params[:id])
   end
 
   def new
