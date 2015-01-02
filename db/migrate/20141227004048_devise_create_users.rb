7f6d892d89efcd6a54dcd6d8c384e964ca7dbe53
technologyRiff <technologyriff@gmail.com>
2015-01-02 15:02:54 -0500

    fix migration

--- db/migrate/20141227004048_devise_create_users.rb
+++ db/migrate/20141227004048_devise_create_users.rb
@@ -36,10 +36,11 @@ class DeviseCreateUsers < ActiveRecord::Migration
 
       t.timestamps
 
+    end
+  
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
-    end
   end
 end
