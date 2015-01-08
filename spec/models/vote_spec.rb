ff52b791abf7bb004b2762d84826e912716dafb3
technologyRiff <technologyriff@gmail.com>
2015-01-08 18:33:50 -0500

    vote_spec

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,40 +1,29 @@
-#require 'rails_helper'
+require 'rails_helper'
 
 describe Vote do
   describe "validations" do
+
+      before do
+        @vote = Vote.create(value: 'vote value')
+        3.times { @vote.create(value: 1) }
+        2.times { @vote.create(value: -1) }
+      end
+
     describe "value validation" do
       it "only allows -1 or 1 as values" do
-        #your expectations here
+        expect ( @vote ).to eq(1) || expect( @vote ).to eq(-1)
       end
     end
   end
 end
 
-#     before do
-#       @post = Post.create(title: 'post title', body: 'post body')
-#       3.times { @post.votes.create(value: 1) }
-#       2.times { @post.votes.create(value: -1) }
-#     end
 
-#     describe '#up_votes' do
-#       it "counts the number of votes with value = 1" do
-#         expect( @post.up_votes ).to eq(3)
-#       end
-#     end
+# expect().to eq() 
+# v = Vote.new(value: 1)
+# v.valid? #=> true
 
-#     describe '#down_votes' do
-#       it "counts the number of votes with value =-1" do
-#         expect( @post.down_votes ).to eq(2)
-#       end
-#     end
+# v2 = Vote.new(value: -1)
+# v2.valid? #=> true
 
-#     describe '#points' do
-#       it "returns the sum of all down and up votes" do 
-#         expect( @posts.points ).to eq(1) #3 -2
-#       end
-#     end
-#   end
-# end
-
-  
-# end
+# v3 = Vote.new(value: 2)
+# v3.valid? #=> false
