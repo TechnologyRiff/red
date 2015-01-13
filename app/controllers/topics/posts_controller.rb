bfa7f0bc4cb3969d856a204524717cbb9e0bb2c2
technologyRiff <technologyriff@gmail.com>
2015-01-13 12:33:58 -0500

    popular posts working

--- app/controllers/topics/posts_controller.rb
+++ app/controllers/topics/posts_controller.rb
@@ -1,7 +1,7 @@
 class Topics::PostsController < ApplicationController
-  def index
-    @posts = Post.visible_to(current_user).where("posts.created_at > ?", 7.days.ago).paginate(page: params[:page], per_page: 10)
-  end
+  # def index
+  #   @posts = Post.visible_to(current_user).where("posts.created_at > ?", 7.days.ago).paginate(page: params[:page], per_page: 10)
+  # end
 
   def show
      @topic = Topic.find(params[:topic_id])
