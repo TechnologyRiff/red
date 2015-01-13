cfcf731872c0b7787f30c4a21af2a5c8e3ab23a0
technologyRiff <technologyriff@gmail.com>
2015-01-12 21:38:25 -0500

    deleting using Ajax

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  respond_to :html, :js
   # def index
   # end
 
@@ -13,17 +14,18 @@ class CommentsController < ApplicationController
    end
 
   def destroy
-    @topic = Topic.find(params[:topic_id])
-    @post = @topic.posts.find(params[:post_id])
+    @post = Post.find(params[:post_id])
     @comment = @post.comments.find(params[:id])
-
     authorize @comment
+
     if @comment.destroy
       flash[:notice] = "Comment was removed."
-      redirect_to [@topic, @post]
     else
       flash[:error] = "Comment couldn't be deleted. Try again."
-      redirect_to [@topic, @post]
     end
+
+      resond_with(@comment) do |format|
+        format.html { redirect_to [@post.topic, @post] }
+      end
   end
 end
